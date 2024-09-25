70624e95a1b34541ea78dcf309fec880778815aa
htolera <htolera@emich.edu>
2024-09-25 16:32:35 -0400

    Programmer 2 change programmer one

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
         System.out.println("Hi");
-        System.out.println("programmer 2 did this");
+        System.out.println("programmer 2 did this. Programmer One did this.");
 	}
 
 }
